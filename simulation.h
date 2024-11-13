6bd1f5de69d21129dd29ba6971641cf00624a87d
jlopezpr <jorge.lopez69@upr.edugit config --list>
2024-11-13 18:14:45 -0400

    simulation.h rehaul, readability and efficiency

--- simulation.h
+++ simulation.h
@@ -3,38 +3,41 @@
 
 #include "init.h"
 #include "hardsphere.h"
-#include "yukawa.h"
 #include "asakuraoosawa.h"
+#include "yukawa.h"
 
-void simulation(global& g, hardsphere& h, yukawa& y, asakuraoosawa& a){
+void simulation(global& g, hardsphere& h, asakuraoosawa& a, yukawa& y){
 
     initialization(g);
-    HJinit(h, g);
-    AOinit(a, g);
-    YKinit(y, g);
 
-    generateNeighborList(h, g);
-    asakuraoosawaNeighborList(a, g); 
-    yukawaNeighborList(y, g);
+    HSinit(g, h);
+    AOinit(g, a);
+    YKinit(g, y);
 
-    std::cout << "Simulation has started" << std::endl;
+    g.forces.resize(N);
 
-    for (int i = 0; i < numberOfTimeSteps; ++i){
-        generateForces(h, g);
-        asakuraoosawaForces(a, g);
-        yukawaForces(y, g);
+    hsNeighborLists(g, h);
+    aoNeighborLists(g, a);
+    ykNeighborLists(g, y);
 
-        updater(a, g);
-        
-        if (i % 100 == 99) {
-            generateNeighborList(h, g);
-            asakuraoosawaNeighborList(a, g);
-            yukawaNeighborList(y, g);
+    for (int i = 0; i < timeSteps; ++i){
+        aoForces(g, a);
+        ykForces(g, y);
+        hsForces(g, h);
+
+        updater(g);
+
+        if (i % 50 == 49) {
+            ykNeighborLists(g, y);
+            hsNeighborLists(g, h);
+            aoNeighborLists(g, a);
         }
-        if ((i + 1) % writtingRate == 0){
-            g.timeStep = i;
+
+        if ((i + 1) % writingRate == 0){
+            g.timeStep = i + 1;
             updateOVITOFile(g);
         }
     }
 }
+
 #endif
